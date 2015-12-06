eff900598d726f19f75c56e5bbb3764fc0769edf
jdelgadop <jdelgadop@taxislibres.com.co>
2015-12-06 14:24:04 -0500

    al dia perrito

--- PruebasRepo/src/addpkg/PruebaAdd.java
+++ PruebasRepo/src/addpkg/PruebaAdd.java
@@ -4,7 +4,7 @@ public class PruebaAdd {
 
 	public static void main(String[] args) {
 		System.out.println("2 do pkg");
-		System.out.println("No Commit");
+		System.out.println("Rama 1");
 	
 	}
 }
--- PruebasRepo/src/addpkg/PruebaAdd2.java
+++ PruebasRepo/src/addpkg/PruebaAdd2.java
@@ -4,7 +4,7 @@ public class PruebaAdd2 {
 
 	public static void main(String[] args) {
 		System.out.println("3 do pkg");
-		System.out.println("No Commit");
+		System.out.println("Rama 2");
 	
 	}
 }
